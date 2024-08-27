54dd5a7d225277e422b92f45f2be096ef9d3c960
binjin0 <a032875@gmail.com>
2024-08-27 05:10:06 +0000

    cicid:배포 테스트

---
+++ api/library.js
@@ -0,0 +1,20 @@
+// import axios from "axios";
+// const BASE_URL = import.meta.env.VITE_LIBRARY_API_URL;
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "http://openapi.seoul.go.kr:8088/5345774a62613033383047496e7270/json/SeoulPublicLibraryInfo/1/1000/"
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json(); // 응답을 JSON으로 변환
+    res.status(200).json(data); // 데이터를 클라이언트로 반환
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching data", error: error.message });
+  }
+}
--- src/components/LibraryData.jsx
+++ src/components/LibraryData.jsx
@@ -7,11 +7,14 @@ const LibraryData = () => {
   useEffect(() => {
     const loadLibraries = async () => {
       try {
-        const data = await fetchLibraries();
-        setLibraries(data.SeoulPublicLibraryInfo.row);
-        console.log("도서관 데이터:", data.SeoulPublicLibraryInfo.row);
+        const res = await fetch("/api/library");
+        const res2 = await fetch("/api/popular");
+        const data = await res.json();
+        const data2 = await res2.json();
+        console.log(data, "도서관 위치");
+        console.log(data2, "인기있는 도서");
       } catch (error) {
-        console.error("Error loading libraries:", error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -20,9 +23,9 @@ const LibraryData = () => {
 
   return (
     <div>
-      {libraries.map((lib) => (
+      {/* {libraries.map((lib) => (
         <div key={lib.LBRRY_SEQ_NO}>{lib.LBRRY_NAME}</div>
-      ))}
+      ))} */}
     </div>
   );
 };
