f900058efb0923cdcf4ae50be5af3a6bcbd00153
binjin0 <a032875@gmail.com>
2024-08-27 06:45:30 +0000

    cicd:pwa중간확인을 위한 주석 비활성화

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,52 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({
-    //   manifest: {
-    //     name: "library-app",
-    //     short_name: "library",
-    //     description: "My app increases the use of youth library",
-    //     theme_color: "#ffffff",
-    //     display: "standalone",
-    //     icons: [
-    //       {
-    //         src: "/logo192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/logo512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //   },
-    //   registerType: "autoUpdate", // 서비스 워커 자동 업데이트
-    //   workbox: {
-    //     // 캐싱 전략을 설정할 수 있습니다.
-    //     runtimeCaching: [
-    //       {
-    //         urlPattern: /^https:\/\/example.com\/.*\/*.json$/,
-    //         handler: "NetworkFirst",
-    //         options: {
-    //           cacheName: "api-cache",
-    //           expiration: {
-    //             maxEntries: 10,
-    //             maxAgeSeconds: 60 * 60 * 24 * 30, // 30일
-    //           },
-    //         },
-    //       },
-    //     ],
-    //   },
-    //   devOptions: {
-    //     enabled: true, // 개발 모드에서 PWA를 활성화
-    //     type: "module", // 서비스 워커가 최신 모듈 시스템을 사용
-    //   },
-    // }),
+    VitePWA({
+      manifest: {
+        name: "library-app",
+        short_name: "library",
+        description: "My app increases the use of youth library",
+        theme_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      registerType: "autoUpdate", // 서비스 워커 자동 업데이트
+      workbox: {
+        // 캐싱 전략을 설정할 수 있습니다.
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/example.com\/.*\/*.json$/,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "api-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30일
+              },
+            },
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true, // 개발 모드에서 PWA를 활성화
+        type: "module", // 서비스 워커가 최신 모듈 시스템을 사용
+      },
+    }),
   ],
   server: {
     hmr: true,
