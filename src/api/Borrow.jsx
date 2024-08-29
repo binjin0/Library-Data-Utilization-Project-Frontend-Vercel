6b767aa0174117ec0296eb4b839e27d7133b1657
binjin0 <a032875@gmail.com>
2024-08-29 09:25:09 +0000

    chore:대출하기 api호출 바디 수정

--- src/api/Borrow.jsx
+++ src/api/Borrow.jsx
@@ -4,9 +4,13 @@ const BASE_URL = import.meta.env.VITE_BACK_URL;
 //대출하기 인증
 export const PostLoan = async (navigate) => {
   try {
-    const response = await axios.post(`${BASE_URL}/borrow_info`, {
-      withCredentials: true,
-    });
+    const response = await axios.post(
+      `${BASE_URL}/borrow_info`,
+      {},
+      {
+        withCredentials: true,
+      }
+    );
     if (response.status === 200) {
       alert("대출인증이 완료되었습니다.");
       navigate(-1);
