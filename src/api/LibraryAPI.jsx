c6f86500d0388d610f293141cb918caacf5aa348
binjin0 <a032875@gmail.com>
2024-09-05 13:20:56 +0000

    chore:api파일 수정

--- src/api/LibraryAPI.jsx
+++ src/api/LibraryAPI.jsx
@@ -1,25 +1,12 @@
 import axios from "axios";
-const BASE_URL = import.meta.env.VITE_LIBRARY_URL;
-// export const fetchLibraries = async () => {
-//   try {
-//     const response = await axios.get(`${BASE_URL}`);
-//     console.log("도서관 정보 데이터", response.data);
-//     return response.data; // 응답 데이터 반환
-//   } catch (error) {
-//     console.error("Failed to fetch librariesAPI:", error);
-//     throw error; // 에러를 호출자에게 전달
-//   }
-// };
+const BASE_URL = import.meta.env.VITE_LIBRARY_API_URL;
 export const fetchLibraries = async () => {
   try {
-    const response = await fetch(`${BASE_URL}`);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    console.log("인기도서대출 데이터:", data);
-    return data;
+    const response = await axios.get(`${BASE_URL}`);
+    console.log("도서관 정보 데이터", response.data);
+    return response.data; // 응답 데이터 반환
   } catch (error) {
-    console.log("Failed to fetch PopularBooks:", error);
+    console.error("Failed to fetch librariesAPI:", error);
+    throw error; // 에러를 호출자에게 전달
   }
 };
--- src/api/PopularBookAPI.jsx
+++ src/api/PopularBookAPI.jsx
@@ -1,27 +1,11 @@
-// import axios from "axios";
-// const BASE_URL = import.meta.env.VITE_POPULAR_BOOK_API_URL;
-
-// export const fetchPopularBooksAPI = async () => {
-//   try {
-//     const response = await axios.get(`${BASE_URL}`);
-//     console.log("인기도서대출 데이터:", response.data);
-//     return response.data;
-//   } catch (error) {
-//     console.log("Failed to fetch PopularBooks:", error);
-//   }
-// };
 import axios from "axios";
 const BASE_URL = import.meta.env.VITE_POPULAR_BOOK_API_URL;
 
 export const fetchPopularBooksAPI = async () => {
   try {
-    const response = await fetch(`${BASE_URL}`);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    console.log("인기도서대출 데이터:", data);
-    return data;
+    const response = await axios.get(`${BASE_URL}`);
+    console.log("인기도서대출 데이터:", response.data);
+    return response.data;
   } catch (error) {
     console.log("Failed to fetch PopularBooks:", error);
   }
